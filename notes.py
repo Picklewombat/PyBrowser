d24acf77df430264f732e2daf5a33fe56d903c72
Picklewombat <134139156+Picklewombat@users.noreply.github.com>
2025-03-01 18:49:43 -0500

    Update notes.py

--- notes.py
+++ notes.py
@@ -5,12 +5,12 @@ print("Start typing your notes below. Type 'SAVE' to save and exit.\n")
 
 notes = []
 
-while True:
-    line = input("|")
-    if line.upper() == "SAVE":
-        with open("notes.txt", "w") as file:
-            file.write("\n".join(notes))
-        print("\nNotes saved to 'notes.txt'. Goodbye!")
-        break
-    else:
-        notes.append(line)
+#while True:
+    #line = input("|")
+    #if line.upper() == "SAVE":
+       # with open("notes.txt", "w") as file:
+         #   file.write("\n".join(notes))
+       # print("\nNotes saved to 'notes.txt'. Goodbye!")
+      #  break
+  #  else:
+      #  notes.append(line)
